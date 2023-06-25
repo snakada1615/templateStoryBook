e6b803764ceb8b614b9a775355c06731526bb856
shunichi <khf04440@nifty.com>
2023-06-25 17:15:15 +0900

    add storybook*Quasar

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,17 @@
 
 <template>
   <div id="app">
-    <q-btn color="primary" label="Primary" />
-    <QBtn color="secondary" label="Secondary" />
-    <LazyQBtn color="amber" glossy label="Amber" />
-    <NuxtLink to="/vueTypes"> vueTypes </NuxtLink>
+    <q-btn color="primary" class="q-mx-lg-md">
+      <div>Primary</div>
+      <NuxtLink to="/"> top </NuxtLink>
+    </q-btn>
+    <q-btn color="secondary" class="q-mx-lg-md">
+      <div>Secondary</div>
+      <NuxtLink to="/vueTypes"> vueTypes </NuxtLink>
+    </q-btn>
+    <q-btn color="amber" class="q-mx-lg-md">
+      <div>Tertiary</div>
+      <NuxtLink to="/test2"> test2</NuxtLink>
+    </q-btn>
   </div>
 </template>
