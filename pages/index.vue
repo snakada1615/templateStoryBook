2044f095fbca3007111a235113a43cfcb4228157
shunichi <khf04440@nifty.com>
2023-06-29 10:02:46 +0900

    add Quasar-Sass

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,13 @@
 
 <template>
   <div id="app">
-    <q-btn color="primary" class="q-mx-lg-md">
-      <div>Primary</div>
-      <NuxtLink to="/"> top </NuxtLink>
-    </q-btn>
-    <q-btn color="secondary" class="q-mx-lg-md">
-      <div>Secondary</div>
-      <NuxtLink to="/vueTypes"> vueTypes </NuxtLink>
-    </q-btn>
-    <q-btn color="amber" class="q-mx-lg-md">
-      <div>Tertiary</div>
-      <NuxtLink to="/test2"> test2</NuxtLink>
-    </q-btn>
+    <q-btn color="primary" class="q-mx-lg-md" to="/" label="Top" />
+    <q-btn
+      color="secondary"
+      class="q-mx-lg-md"
+      to="/vueTypes"
+      label="vueTypes"
+    />
+    <q-btn color="amber" class="q-mx-lg-md" to="/test2" label="test2" />
   </div>
 </template>
--- pages/test2.vue
+++ pages/test2.vue
@@ -4,12 +4,21 @@ import TestVue from "~/components/atoms/testVue.vue";
 
 <template>
   <div id="app">
-    <q-btn color="secondary" class="q-mx-md">
-      <NuxtLink to="/vueTypes"> vueTypes </NuxtLink>
-    </q-btn>
-    <q-btn color="secondary" class="q-mx-md">
-      <NuxtLink to="/"> index </NuxtLink>
-    </q-btn>
+    <q-btn color="primary" class="q-mx-lg-md" to="/" label="Top" />
+    <q-btn
+      color="secondary"
+      class="q-mx-lg-md"
+      to="/vueTypes"
+      label="vueTypes"
+    />
+    <div class="q-mt-md q-gutter-md">
+      <q-icon name="font_download" color="primary" size="32px" />
+      <q-icon name="warning" color="warning" size="4rem" />
+      <q-icon name="format_size" style="color: #ccc; font-size: 1.4em" />
+      <q-icon name="print" color="teal" size="4.4em" />
+      <q-icon name="today" class="text-orange" size="2em" />
+      <q-icon name="style" size="3em" />
+    </div>
     <test-vue :task="{ id: 'hiroto', state: false, title: 'hey guys' }" />
   </div>
 </template>
